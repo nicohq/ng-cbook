7733f0ed0e40ac6a2ed2983aa72539976d0eb532
Nico Trygub <nico@alterpost.org>
2017-09-14 11:55:23 +0300

    FE: pagination

--- client/app/components/contacts/contacts.component.js
+++ client/app/components/contacts/contacts.component.js
@@ -9,8 +9,12 @@
             controller: ContactsCtrl
         });
 
-    function ContactsCtrl() {
-        var $ctrl = this;
+    ContactsCtrl.$inject = ['APIService'];
+
+    function ContactsCtrl(APIService) {
+        var $ctrl = this,
+            contactLimit = 10,
+            skip = 10;
 
         $ctrl.replaceContactInList = function(contact) {
             $ctrl.list.data.forEach(function(origin, idx, array) {
@@ -24,6 +28,43 @@
             $ctrl.list.data = $ctrl.list.data.filter(function(contact) {
                 return contact._id !== id;
             });
+        };
+
+        $ctrl.listPrev = function() {
+            var params = {};
+
+            if(skip >= 0) {
+                angular.extend(params, {
+                    skip: skip,
+                    limit: contactLimit
+                });
+
+                APIService.getContacts(params)
+                    .then(function(response) {
+                        var length = response.data.length;
+                        skip = skip - length;
+                        $ctrl.list = response;
+                    })
+                    .catch(console.error.bind(console, 'Error while requesting contacts :'));
+            }
+        };
+
+        $ctrl.listNext = function() {
+            var params = {};
+            if(skip % contactLimit === 0) {
+                angular.extend(params, {
+                    limit: contactLimit,
+                    skip: skip
+                });
+
+                APIService.getContacts(params)
+                    .then(function(response) {
+                        var length = response.data.length;
+                        skip = skip + length;
+                        $ctrl.list = response;
+                    })
+                    .catch(console.error.bind(console, 'Error while requesting contacts :'));
+            }
         };
 
     }
